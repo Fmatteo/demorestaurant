3a86a3e719d3a1f37cbc18b6cca6c6c8356b98b5
Fmatteo <markrecovery23@gmail.com>
2018-12-03 01:56:20 +0800

    nice

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -66,15 +66,15 @@
           <div class="col-lg-3 col-xs-6">
             <!-- small box -->
             <div class="small-box bg-red">
-              <div class="inner">
-                <h3><?php echo $total_stores ?></h3>
+              <div class="inner" style="padding:25px;">
+                <h3>TOTAL SALES</h3>
 
-                <p>Total Stores</p>
+                
               </div>
               <div class="icon">
                 <i class="ion ion-android-home"></i>
               </div>
-              <a href="<?php echo base_url('stores/') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              <a href="<?php echo base_url('reports/') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
